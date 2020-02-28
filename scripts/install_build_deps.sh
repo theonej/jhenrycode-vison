40f118577352023f900fbec79b61dfca5980850e
ultralord <j.henry@n-dexed.com>
2020-02-27 21:23:34 -0500

    fixing typeos

--- scripts/install_build_deps.sh
+++ scripts/install_build_deps.sh
@@ -1,15 +1,15 @@
 apt-get clean
-ap-get update
+apt-get update
 apt-get install python-pip -y
 pip install awscli
 apt-get install jq sudo unzip git curl wget -y
 
 sudo apt-get install software-properties-common -y
 sudo apt-get update
-
+     
 apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionci stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 apt update
 apt-cache policy docker-ce
 apt install docker-ce -y
