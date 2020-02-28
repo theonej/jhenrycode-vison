4d393fd130d6218edbde69191e6feab57bdf7fae
ultralord <j.henry@n-dexed.com>
2020-02-27 21:18:39 -0500

    adding docker file for deploying service

--- infrastructure/stage/stage.tf
+++ infrastructure/stage/stage.tf
@@ -24,12 +24,12 @@ terraform {
 }
 
 module "vision-predictions" {
-    source = "git@github.com:theonej/jhenrycode-infrastructure.git//module/ecs"
+    source = "git@github.com:theonej/jhenrycode-infrastructure.git//modules/ecs"
 
     security_group_id = data.terraform_remote_state.jhenrycode-vision.outputs.security_group_id
     subnet_ids = data.terraform_remote_state.jhenrycode-vision.outputs.subnet_ids
     target_group_arn = data.terraform_remote_state.jhenrycode-vision.outputs.target_group_arn
     ecs_cluster_id = data.terraform_remote_state.jhenrycode-vision.outputs.ecs_cluster_id
-    
+
     tag = "${var.tag}"
 }
